5852fee2bc60f8ece5fef066a9eff9755c2f9c44
randomite <ravin.sardal@gmail.com>
2020-09-12 13:09:11 -0700

    first commit

---
+++ Derminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Derminder
+//
+//  Created by Ravin Personal on 9/11/20.
+//  Copyright © 2020 Ravin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
